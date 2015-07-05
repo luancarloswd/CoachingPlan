d9951bf9315f34256682dca5b91927ed9e53f8f3
luancarloswd <luancarloswd@gmail.com>
2015-07-05 16:03:31 -0300

    Revert "Exclusão tabelas Identity"
    This reverts commit 7b8546f83a3a1fde280e162396c280608dcd717c.

---
+++ CoachingPlan.Domain/Models/Usuario_Papel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoachingPlan.Domain.Models
+{
+    public class Usuario_Papel
+    {
+        public string ID_USUARIO { get; set; }
+        public string ID_PAPEL { get; set; }
+        public string IDENTITYUSER_ID { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+        public virtual Papel Papel { get; set; }
+    }
+}
