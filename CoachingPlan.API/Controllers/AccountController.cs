425996b2347e6d995c318a3cff6c825d284adb36
luancarloswd <luancarloswd@gmail.com>
2015-07-08 14:55:16 -0300

    Exclusão projeto de testes

---
+++ CoachingPlan.API/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using CoachingPlan.Domain.Contracts.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace CoachingPlan.API.Controllers
+{
+    public class AccountController
+    {
+     
+    }
+}
